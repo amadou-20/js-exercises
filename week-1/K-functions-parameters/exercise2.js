73cd0647b702b4c92531fd9c2c969a97072feb69
amadou saidy <ahmo300@outlook.com>
2020-04-04 18:52:07 +0200

    i did the K-functions-parameters exercise3.js

--- week-1/K-functions-parameters/exercise2.js
+++ week-1/K-functions-parameters/exercise2.js
@@ -1,5 +1,9 @@
 // Declare your function first
 
+function divide(a,b){
+    return 3 / 4;
+}
+
 var result = divide(3, 4);
 
 console.log(result);
--- week-1/K-functions-parameters/exercise3.js
+++ week-1/K-functions-parameters/exercise3.js
@@ -1,5 +1,13 @@
 // Write your function here
 
-var greeting = createGreeting("Daniel");
+function createGreeting(a, b){
+    return greetingword + name;
+}
+
+var name = "Amadou";
+ var greetingword = "hello, my name is ";
+
+
+var greeting = createGreeting(greetingword, name);
 
 console.log(greeting);
