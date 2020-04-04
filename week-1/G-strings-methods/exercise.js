34d22b3ba481e537726a990654d3a5327c4a7b3c
amadou saidy <ahmo300@outlook.com>
2020-04-04 02:36:26 +0200

    i did the exercise G-strings-methods

--- week-1/G-strings-methods/exercise.js
+++ week-1/G-strings-methods/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
 
-console.log(message);
+var name = "amadousaidy";
+
+console.log("my name is " + name + " " + "and my name is" + " "+ name.length +" "+ "characters long.");
