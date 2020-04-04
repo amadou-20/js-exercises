70cb9bdedb1dbdcaa0e24034fec98676d7405844
amadou saidy <ahmo300@outlook.com>
2020-04-04 17:08:18 +0200

    i did the J-functions

--- week-1/J-functions/exercise.js
+++ week-1/J-functions/exercise.js
@@ -1,6 +1,16 @@
 function halve(number) {
-  // complete the function here
+  return number / 2;
 }
+
+function sum(number) {
+  return number + 20;
+}
+
+var total = sum(12);
+
+console.log(total);
+
+
 
 var result = halve(12);
 
