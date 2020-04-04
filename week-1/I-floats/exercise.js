e132ff4bea33bdfb64f1fbbcdf41c84dec25a570
amadou saidy <ahmo300@outlook.com>
2020-04-04 03:52:13 +0200

    id did the I-floats exercise

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,11 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+var percentageofstudents = numberOfStudents * 100 / 23;
+var studentsrounded = Math.round(numberOfStudents * 100 / 23);
+
+var percentageofmentors = numberOfMentors * 100 / 23;
+var mentorsrounded = Math.round(numberOfMentors * 100 / 23);
+
+console.log("percentage students:" + percentageofstudents + "%");
+console.log("percentage mentors:" + percentageofmentors + "%");
