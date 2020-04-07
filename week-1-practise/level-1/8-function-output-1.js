3a67e76098aab2800ee4923b2f9b058113f1784f
amadou saidy <ahmo300@outlook.com>
2020-04-07 04:11:32 +0200

    i checked the exercise level-1 errors

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -7,6 +7,13 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do?
+this program give a random Number every times it executed
+
 // - How many parameters does the function take?
+this function does not take parameters
+
 // - What is the function name?
+the name of this is "getnumber"
+
 // - Where is the function called? with what parameters?
+the function is called at line 5 without parameters.
--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -7,8 +7,19 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do? Run it and see, or Google some of the methods used ot understand them.
+this program concatenate the string words
+
 // - How many parameters does the function take?
+this function take two parameters
+
 // - What is the function name?
+the name of this is : s
+
 // - Where is the function called? with what parameters?
+the function is called at line 5  withe 'code' and 'your' parameters
+
 // - What does the function return?
+this function return a string
+
 // - How can this function be improved?
+we can define the 'code' and 'your' parameters inside of function.
