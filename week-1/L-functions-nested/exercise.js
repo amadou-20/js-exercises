8dadc58071ef5c9e782bf4e1b1067c8ade6440cd
amadou saidy <ahmo300@outlook.com>
2020-04-08 02:56:03 +0200

    I did the L-functions-nested exercise.js

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -3,3 +3,33 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+ 
+// the program
+
+let numberofstudents = 15;
+let numberofmentors = 8;
+numberofpeople = 23;
+
+// function of the students
+
+function createpercentagestudents(){
+    return numberOfstudents * 100 / numberofpeople;
+}
+
+function createmessage(){
+    let percentageofstudents = createpercentagestudents();
+    let message = " percentage students : " + percentageofstudents;
+    return message;
+}
+
+// function of the mentors
+
+function createpercentagementors(){
+    return numberOfmentors * 100 / numberofpeople;
+}
+
+function creatementorsmessage(){
+    let percentageofmentors = createpercentagestudents();
+    let messagementors = " percentage mentors : " + percentageofmentors;
+    return messagementors;
+}
