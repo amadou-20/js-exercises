eaa0cf20feae3684648cfede0125304047841902
amadou saidy <ahmo300@outlook.com>
2020-04-11 00:46:22 +0200

    i change the the B-boolean-literals exercise.js

--- week-2/B-boolean-literals/exercise.js
+++ week-2/B-boolean-literals/exercise.js
@@ -10,10 +10,16 @@ var codeYourFutureIsGreat = true;
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
-
+  
 console.log("Is Code Your Future great?", codeYourFutureIsGreat);
+
+let mozafarIsCool = false;
 console.log("Is Mozafar cool?", mozafarIsCool);
+
+let calculationCorrect = true;
 console.log("Does 1 + 1 = 2?", calculationCorrect);
+
+let  moreThan10Students = false;
 console.log("Are there more than 10 students?", moreThan10Students);
 
 /* 
